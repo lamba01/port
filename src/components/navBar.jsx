2cc5db91b5323b600fa19347a7f9e0a3a51a1553
lamba01 <jamesspaul987@gmail.com>
2025-03-28 13:51:18 +0100

    added top navigation bar

---
+++ src/components/navBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import john from "../assets/john-oluwafemi.jpeg";
+
+export default function navBar() {
+  return (
+    <menu className="flex w-full justify-between items-center px-5 sm:px-10 backdrop-blur-3xl h-[50px]">
+      <div className="flex items-center">
+        <img
+          className="h-[40px] w-[40px] rounded-xl border-2 border-white shadow-md"
+          src={john}
+          alt=""
+        />
+        <h5 className="font-bold">John</h5>
+      </div>
+      <div className="flex gap-5 md:gap-10 capitalize">
+        <a className="text-sm md:text-lg" href="/about">
+          about
+        </a>
+        <a className="text-sm md:text-lg" href="/work">
+          work
+        </a>
+        <a className="text-sm md:text-lg" href="projects">
+          projects
+        </a>
+      </div>
+    </menu>
+  );
+}
