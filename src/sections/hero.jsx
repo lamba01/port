d0219aea82cd6e059017292f956263279c72a4a9
lamba01 <jamesspaul987@gmail.com>
2025-04-05 18:25:36 +0100

    edited hero section

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -7,7 +7,7 @@ export default function Hero({ scrollToProjects }) {
       <img
         src={john}
         alt="Profile"
-        className="absolute top-[42%] left-1/2 -translate-x-1/2 -translate-y-1/2 h-[130px] w-[130px] md:w-[180px] md:h-[180px] object-cover rounded-full opacity-100 border-8 border-solid border-white z-10"
+        className="absolute top-[42%] left-1/2 -translate-x-1/2 -translate-y-1/2 h-[130px] w-[130px] md:w-[180px] md:h-[180px] object-cover rounded-full opacity-100 border-8 border-solid border-white z-10 shadow-lg hover:shadow-[0_0_20px_rgba(59,130,246,0.6)] transition transform hover:scale-105 "
       />
       <div className="flex flex-col items-center gap-15 justify-center">
         <h1 className="text-6xl md:text-9xl uppercase" data-aos="fade-left">
@@ -23,7 +23,7 @@ export default function Hero({ scrollToProjects }) {
       <p className="text-lg font-medium text-center md:text-xl my-4">
         Coding my way through challenges to build something amazing.
       </p>
-      <div className="flex flex-col md:flex-row w-full items-center justify-center mt-5 gap-3">
+      <div className="flex flex-col md:flex-row w-full items-center justify-center mt-10 sm:mt-5 gap-3">
         <a href="mailto:moyinooluwafemi2004@gmail.com">
           <button className="primary" data-aos="fade-up">
             lets work
