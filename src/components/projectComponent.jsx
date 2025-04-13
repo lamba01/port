a5caeed2f831415be7bda310408a1d67574d249e
lamba01 <jamesspaul987@gmail.com>
2025-04-13 16:26:51 +0100

    fixed overlapping issue

--- src/components/projectComponent.jsx
+++ src/components/projectComponent.jsx
@@ -86,7 +86,7 @@ const projectsData = [
 
 export default function Projects({ limit }) {
   return (
-    <section className="bg-[#f5f5f5] py-[50px] px-[20px] sm:px-[40px] w-full text-start">
+    <section className="bg-[#f5f5f5] py-[50px] px-[20px] sm:px-[40px] text-start overflow-x-hidden">
       <h2>{limit ? "Projects" : "Explore my work👀"}</h2>
       <p>Some of the noteworthy projects I have built</p>
       <main className="flex flex-wrap justify-center gap-10 my-6 text-start">
@@ -124,9 +124,9 @@ export default function Projects({ limit }) {
         ))}
         {limit && (
           <Link to={`/projects`}>
-            <button className="bg-black text-white cursor-pointer py-3 px-8 sm:w-[250px] w-[90vw] hover:bg-[#333333]">
+            {/* <button className="bg-black text-white cursor-pointer py-3 px-8 sm:w-[250px] w-[90vw] hover:bg-[#333333]">
               view all projects{" "}
-            </button>
+            </button> */}
           </Link>
         )}
       </main>
